3f3795eb0c1ff29696afa4d5c95c9dd303ff3696
pankov <pankov@tutu.tech>
2026-02-23 18:04:36 +0300

    refactor bee::dest

--- src/activity/mod.rs
+++ src/activity/mod.rs
@@ -2,29 +2,20 @@ use ggez::glam::{vec2, Vec2};
 
 pub struct Activity {
     speed: f32,
-    started: bool,
     pos: Vec2
 }
 
 impl Activity {
-    pub fn new(speed: f32, started: bool, pos: Vec2) -> Self {
-        Self{speed , started, pos}
-    }
-
-    pub fn speed(&self) -> f32 {
-        self.speed
-    }
-
-    pub fn started(& self) -> bool {
-        self.started
+    pub fn new(speed: f32, pos: Vec2) -> Self {
+        Self{speed, pos}
     }
 
     pub fn pos(&self) -> Vec2 {
         self.pos
     }
 
-    pub fn sub_x(&mut self, modifier: f32) {
-        self.pos = vec2(self.pos.x - modifier, self.pos.y);
+    pub fn move_to_left(&self, modifier: f32) -> Activity {
+        Activity{speed: self.speed, pos: vec2(self.pos.x - (self.speed * modifier), self.pos.y)}
     }
 
     pub fn reached_end_of_window(&self, limit: f32) -> bool {
--- src/object/bee/mod.rs
+++ src/object/bee/mod.rs
@@ -5,14 +5,14 @@ use ggez::graphics::Image;
 use rand::random_range;
 
 pub struct Bee {
-    activity: Activity,
+    activity: Option<Activity>,
     image: Image,
 }
 
 impl Bee {
     pub fn new(image: Image) -> Self {
         Self {
-            activity: Activity::new(0.0, false, vec2(0.0, 0.0)),
+            activity: None,
             image,
         }
     }
@@ -22,19 +22,22 @@ impl Bee {
     }
 
     pub fn dest(&mut self, ctx: &mut Context) -> Vec2 {
-        if !self.activity.started() {
-            self.activity = Activity::new(
-                random_range(200.0..=400.0),
-                true,
-                vec2(2000.0, random_range(500.0..=999.0)),
-            );
-        } else {
-            self.activity
-                .sub_x(self.activity.speed() * ctx.time.delta().as_secs_f32());
-            if self.activity.reached_end_of_window(-(self.image.width() as f32)) {
-                self.activity = Activity::new(0.0, false, vec2(0.0, 0.0));
+        match &self.activity {
+            Some(activity) => {
+                let changed = activity.move_to_left(ctx.time.delta().as_secs_f32());
+                let pos = changed.pos();
+                if activity.reached_end_of_window(-(self.image.width() as f32)) {
+                    self.activity = None;
+                } else {
+                    self.activity = Some(changed);
+                }
+                pos
+            }
+            None => {
+                let pos = vec2(2000.0, random_range(500.0..=999.0));
+                self.activity = Option::from(Activity::new(random_range(200.0..=400.0), pos));
+                pos
             }
         }
-        self.activity.pos()
     }
 }
